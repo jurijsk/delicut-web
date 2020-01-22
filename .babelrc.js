00639a0dcd9f31132ea2aa95a0526358995fe04d
Jurijs Kovzels <jurijs.kovzels@outlook.com>
2020-01-22 16:33:43 +0100

    minor stuff

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
-	"plugins": [
-		["babel-plugin-styled-components", {
-			"pure": true
-		}]
-	]
+	presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+	//This aint working because just (https://github.com/microsoft/just) just did not add babel config
+	// plugins: [
+	// 	["babel-plugin-styled-components", {
+	// 		"ssr": false,
+	// 		"pure": true,
+	// 		"displayName": true,
+	// 		"minify": false
+	// 	}]
+	// ],
+	// debug: true
 };
