faabed9bfed680ee804b197f3fbad9eb2653de78
Jurijs Kovzels <jurijs.kovzels@outlook.com>
2020-03-19 16:07:48 +0100

    adding gtm tag

--- src/Application.tsx
+++ src/Application.tsx
@@ -30,6 +30,17 @@ export const Application: React.FunctionComponent = () => {
 				<Helmet>
 					<title>{t("Delicut - Make your podcast seen and listened")}</title>
 					<meta name="description" content={t("Delicut allows talk-audio producers to create short engaging video cuts using audio and transcription which work even when muted")} />
+					
+					{/* Global site tag (gtag.js) - Google Analytics  */}
+					<script async src='https://www.googletagmanager.com/gtag/js?id=UA-149833499-1'></script>
+					<script>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'UA-149833499-1');
+					`}
+					</script>
 				</Helmet>
 				<AppHeader></AppHeader>
 				<StyledMain>
